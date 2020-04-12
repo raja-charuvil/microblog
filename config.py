879ceb1a1f37ce7a3a8188623033af68bd81424b
rach62 <raja.asokan@gmail.com>
2020-04-12 12:39:56 +0200

    add database sqlite

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
 	SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
