be6b58d813aab1e138cb8353f5606ec1373e85ed
rach62 <raja.asokan@gmail.com>
2020-04-23 12:23:57 +0200

    add email handler

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ class Config(object):
 	SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
 	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+	# Configs for Email notification for 500 errors
+	MAIL_SERVER = os.getenv('MAIL_SERVER', 'default-mail-server')
+	MAIL_PORT = os.getenv('MAIL_PORT', 25)
+	MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+	ADMINS = ['sweetytweetypy@gmail.com']
